92693de48d5c601e7dba5392bc9b9a50138c26bf
Nikhil Murmu <nikhil.paul211@gmail.com>
2024-10-04 12:01:02 +0530

    Added SugnedIn logic

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -1,19 +1,19 @@
 "use client"
 import { Button } from '@/components/ui/button'
-import { SignedIn, UserButton } from '@clerk/nextjs'
+import { SignInButton, UserButton, useUser } from '@clerk/nextjs'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
 function Header() {
-   const signedIn = SignedIn
+  const { user, isSignedIn } = useUser();
   return (
     <div className='flex items-center justify-between p-5 shadow-md'>
         <Image src={'/logo.svg'} width={70} height={50}/>
        <div className='flex items-center justify-end gap-2'>
-        {signedIn?<Button><Link href={'/dashboard'}>Dashboard</Link></Button>:<Button>Get Started</Button>}
-       
-       {signedIn?<UserButton/>:""}
+        {isSignedIn?<Button><Link href={'/dashboard'}>Dashboard</Link></Button>:<SignInButton><Button>Get Started</Button></SignInButton>}
+        
+       {isSignedIn?<UserButton/>:""}
        </div>
     </div>
   )
